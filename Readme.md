809fe31585613cb41ea8ba7696196c1b6ff07d0d
Abimanianandan <abikbm06@gmail.com>
2023-10-30 16:23:09 +0530

    Day 5 task

---
+++ Readme.md
@@ -0,0 +1,3 @@
+1.Given JSON iterate overall for loops(for,for in ,for of,for each):
+2.Create own resume data in JSON format:
+3.Diffrence between window,screen and document in javascript:
---
+++ script.js
@@ -0,0 +1,40 @@
+// 1.For the given JSON iterate overall forloops (for,for in, for of,for each)
+const obj=[
+    {
+    "name":"Abimani",
+    "age":"20",
+    "degree":"Bca",
+    "city":"Thanjavur",
+    "hobby":"Movies"
+}
+]
+// For loop:-
+for(var i=0;i<obj.length;i++){
+    console.log(obj[i]);
+}
+
+// For in loop:-
+for(let i in obj){
+    console.log(obj[i]);
+}
+
+//For of loop:-
+for(let a in obj){
+    console.log(obj[a]);
+}
+
+// For each loop:-
+for(let b in obj){
+    console.log(obj[b]);
+}
+
+// 2.Create your own resume data in JSON format:-
+const obj1=[
+    {
+    "name":"Abimani",
+    "age":"20",
+    "degree":"Bca",
+    "city":"Thanjavur",
+    "hobby":"Movies"
+}
+]
